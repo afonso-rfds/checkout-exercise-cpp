b2810dcd1dea7e5392e777ec24e58392b76145fa
afonso-rfds <afonso.rfds@gmail.com>
2024-05-30 17:44:21 +0100

    #Implemented class CItem

--- CItem.cpp
+++ CItem.cpp
@@ -1,1 +1,13 @@
 #include "CItem.h"
+
+char CItem::getSKU()
+{
+	return m_sku;
+}
+
+
+
+int CItem::getPrice()
+{
+	return m_price;
+}
--- CItem.h
+++ CItem.h
@@ -1,5 +1,18 @@
 #pragma once
+
 class CItem
 {
+public:
+	CItem() = delete;
+	~CItem() = default;
+
+	CItem(char f_sku, int f_price) : m_sku(f_sku), m_price(f_price) {}
+
+	char getSKU();
+	int  getPrice();
+
+private:
+	char m_sku;
+	int  m_price;
 };
 
